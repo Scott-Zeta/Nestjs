14bfb368d4a6e8a45ff3a6179d1d30cb6b383248
Scott Zeta <scott-zeta@outlook.com>
2024-11-06 13:21:17 +1030

    map entity to type orm

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() //typeorm decorator to map the entity to a table
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   brand: string;
+  @Column('json', { nullable: true }) // store array in json, allow null
   flavors: string[];
 }
